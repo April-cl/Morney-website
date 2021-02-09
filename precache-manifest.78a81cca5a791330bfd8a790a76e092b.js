0a2f9427919262964e904b8eb0ee22a3ea259b30
April <xcl8202@gmail.com>
2021-02-10 03:24:44 +0800

    deploy

---
+++ precache-manifest.78a81cca5a791330bfd8a790a76e092b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2ee22e5de852009917c",
+    "url": "/Morney-website/css/app.90d82dd7.css"
+  },
+  {
+    "revision": "e4be0f6c3533a633cb7f9c264fc5d18f",
+    "url": "/Morney-website/index.html"
+  },
+  {
+    "revision": "b2ee22e5de852009917c",
+    "url": "/Morney-website/js/app.8aafb0f1.js"
+  },
+  {
+    "revision": "ca98d91145650932869b",
+    "url": "/Morney-website/js/chunk-vendors.1b05312f.js"
+  },
+  {
+    "revision": "34fa58f264e125e51ff260d77309bdbf",
+    "url": "/Morney-website/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/Morney-website/robots.txt"
+  }
+]);
